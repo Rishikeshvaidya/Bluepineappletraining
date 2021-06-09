f66a6d7f7662d16b2632d0a195b211bd6470e1ac
Rishikeshvaidya <66507149+Rishikeshvaidya@users.noreply.github.com>
2021-06-08 21:23:48 -0700

    Update pallindromecheck.py

--- 7 th june/pallindromecheck.py
+++ 7 th june/pallindromecheck.py
@@ -1,1 +1,21 @@
 
+def pallindromecheck(s):
+    l=len(s)
+    if l==0 or l==1:
+        return True
+    else:
+        for i in range(0,l//2):
+            if s[i]!=s[l-1-i]:
+                return False
+        return True
+            
+
+s=input()
+k=s.lower()
+
+x=pallindromecheck(k)
+if x==True:
+    print("pallindrome")
+else:
+    print("Not pallindrome")
+
